af5b5b8bba55e9dec07830808112629c93e78f54
Luciano <luciano.corniglione@gmail.com>
2015-04-14 16:59:56 -0300

    ahora si

--- src/distribucionesrandom/DistribucionUniforme.java
+++ src/distribucionesrandom/DistribucionUniforme.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package distribucionesrandom;
 
-/**
- *
- * @author Luciano
- */
-public class DistribucionUniforme {
+
+public class DistribucionUniforme 
+{
+    private int a;
+    private int b;
+    private double v[];
+    private int cantNum;
+
+    public DistribucionUniforme(int a, int b, double[] v, int cantNum) {
+        this.a = a;
+        this.b = b;
+        this.v = new double[cantNum];
+        this.cantNum = cantNum;
+    }
+
+    public double[] generarDistribucion()
+    {
+        double num = 0;
+        for (int i = 0; i < v.length; i++) 
+        {
+            num = (((b - a) * Math.random()) + a);
+            v[i] = num;
+        }
+        return v;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public double[] getV() {
+        return v;
+    }
+
+    public void setV(double[] v) {
+        this.v = v;
+    }
+
+    public int getCantNum() {
+        return cantNum;
+    }
+
+    public void setCantNum(int cantNum) {
+        this.cantNum = cantNum;
+    }
+    
+    
     
 }
