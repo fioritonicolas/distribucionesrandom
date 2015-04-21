b019eee18475ce2f3b8a88470423b86a3f368b94
martinseco <mseco.93@gmail.com>
2015-04-21 18:05:48 -0300

    Distribucion Normal

--- src/distribucionesrandom/Main.java
+++ src/distribucionesrandom/Main.java
@@ -15,7 +15,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        DistribucionNormal dN = new DistribucionNormal(10.0, 5.0);
+        
+        dN.generarRND(500);
+        
+        System.out.println(dN.toString());
+        
     }
     
 }
