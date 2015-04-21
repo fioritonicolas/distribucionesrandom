5b8ad538281fe1b6a31d13afe894a78e1b11eb2b
Nicolas <fiorito.nicolas@gmail.com>
2015-04-21 17:27:13 -0300

    listo poisson

--- src/distribucionesrandom/Main.java
+++ src/distribucionesrandom/Main.java
@@ -16,6 +16,36 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        
+        
+        DistribucionUniforme dU = new DistribucionUniforme(5, 12, 500);
+        double[] generarDistribucion = dU.generarDistribucion();
+        PoissonDistribucion pD = new PoissonDistribucion(500, 5.3);
+        int[] distribucion = pD.generarDist();
+        int[] frecuencias = new int[20];
+        for (int i = 0; i < distribucion.length; i++) {
+            int e = distribucion[i];
+            frecuencias[e]++;
+            //System.out.println(e);
+            
+        }
+        
+        System.out.println("LOS NUMEROS:");
+        for (int i = 0; i < frecuencias.length; i++) {
+            System.out.println(i);
+            
+        }
+        
+        System.out.println("SUS FRECUENCIAS");
+        for (int i = 0; i < frecuencias.length; i++) {
+            System.out.println(frecuencias[i]);
+            
+        }
+        
+        
+        
+      
     }
     
 }
